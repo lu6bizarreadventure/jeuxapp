b823cb1973b8344e2b1dcd3d1c750e056e3a9895
Lucie <lucie.garofalo@etu.univ-savoie.fr>
2024-01-19 10:33:06 +0100

    Initial Commit

---
+++ jeuxapp/jeuxappApp.swift
@@ -0,0 +1,17 @@
+//
+//  jeuxappApp.swift
+//  jeuxapp
+//
+//  Created by Lucie GAROFALO on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct jeuxappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
